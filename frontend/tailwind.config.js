0e341c1afe66885611af9e298d20703ac37448f3
CMPKishan <kishan@compassconsult.co>
2024-03-06 22:19:45 +1100

    The Weather Panel is done.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,25 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: colors.black,
+            white: colors.white,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            stone: colors.warmGray,
+            sky: colors.lightBlue,
+            neutral: colors.trueGray,
+            gray: colors.coolGray,
+            slate: colors.blueGray,
+            lime: colors.lime,
+            rose: colors.rose,
+        },
     },
     plugins: [],
 };
